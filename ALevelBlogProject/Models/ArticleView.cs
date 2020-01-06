36f29ea68b4e2d37941672eeb8544237a5dc6f11
Kurilo Dmitriy <fobosdem@gmail.com>
2020-01-06 19:37:13 +0200

    Edditing project

--- ALevelBlogProject/Models/ArticleView.cs
+++ ALevelBlogProject/Models/ArticleView.cs
@@ -16,6 +16,7 @@ namespace ALevelBlogProject.Models
 		public string SubTitle { get; set; }
 		public string Body { get; set; }
 		public string Image { get; set; }
+		[HiddenInput(DisplayValue = false)]
 		public int AuthorId { get; set; }
 		[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}",
 	   ApplyFormatInEditMode = true)]
